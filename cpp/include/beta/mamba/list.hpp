c9b15f3cea992fe38bf47981bcaa42a72379762a
antonsynd <nguyen.antony@gmail.com>
2024-07-21 22:59:44 -0400

    list: Add more List<T> APIs

--- cpp/include/beta/mamba/list.hpp
+++ cpp/include/beta/mamba/list.hpp
@@ -42,29 +42,95 @@ class List {
 
   /// @brief Target of `x in y`
   bool contains(T elem) const {
-    return std::find(v_.begin(), v_.end(), elem) != v_.end();
+    return std::find(v_.cbegin(), v_.cend(), elem) != v_.end();
   }
 
   /// @brief Target of `list.clear()`
   void clear() { v_.clear(); }
 
   /// @brief Target of `list.copy()`
-  List<T> copy() { return *this; }
+  List<T> copy() const { return *this; }
 
   /// @brief Target of `list.extend(Iterable)`
   /// @todo Fix to not assume list_t
   template <typename Iterable,
             std::enable_if_t<traits::is_iterable_v<Iterable, T>>>
   void extend(const Iterable<T>& other) {
-    std::copy(other.begin(), other.end(), std::back_inserter(v_));
+    std::copy(other.cbegin(), other.cend(), std::back_inserter(v_));
   }
+
+  void operator+=(const List<T>& other) { this->extend(other); }
 
   /// @brief Target of `x + y`
   List<T> operator+(const List<T>& other) {
-    List<T> res;
+    List<T> res = *this;
 
-    res.emplace
+    res.extend(other);
+
+    return res;
   }
+
+  /// @brief Target of `x * n`
+  List<T> operator*(int_t i) {
+    List<T> res;
+    res.v_.reserve(v_.size() * i);
+
+    for (; i > 0; --i) {
+      res.extend(*this);
+    }
+
+    return res;
+  }
+
+  void operator*=(int_t i) {
+    if (i == 1) {
+      return;
+    } else if (i == 0) {
+      clear();
+    }
+
+    v_.reserve(v_.length() * i);
+
+    for (; i > 1; --i) {
+      this->extend(*this);
+    }
+  }
+
+  /// @todo Implement negative indices
+  T& operator[](int_t idx) { return v_[idx]; }
+
+  /// @todo Implement negative indices
+  T operator[](int_t idx) const { return v_[idx]; }
+
+  int_t len() const { return v_.size(); }
+
+  /// @todo Actually implement
+  T min() const { return v_.front(); }
+  T max() const { return v_.front(); }
+
+  int_t count(T elem) const {
+    return std::count_if(v_.cbegin(), v_.cend(),
+                         [](T val) { return val == elem; });
+  }
+
+  /// @brief Target of `x[i:j(:k)]`
+  /// @todo
+  List<T> slice(int_t start, int_t end, int_t step = 1) const { return *this; }
+
+  /// @tood
+  void replace_slice(const List<T>& other,
+                     int_t start,
+                     int_t end,
+                     int_t step = 1) {}
+
+  /// @brief Target of `x.index(i, (j, (k)))`
+  /// @todo
+  int_t index(T elem, int_t start = 0, int_t end = -1) const { return 0; }
+
+  void insert(int_t idx, T elem) {}
+  void pop(int_t i) {}
+  void remove(T elem) {}
+  void reverse() {}
 
  private:
   std::vector<T> v_;
