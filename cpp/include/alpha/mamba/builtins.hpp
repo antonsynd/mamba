32e42f8729b3e9e2173cf244cba88ad36a5357ab
antonsynd <nguyen.antony@gmail.com>
2024-07-23 20:18:55 -0400

    cpp: Add CMake boilerplate

--- cpp/include/alpha/mamba/builtins.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "mamba/alpha/integer.hpp"
-#include "mamba/alpha/io.hpp"
-#include "mamba/alpha/list.hpp"
-#include "mamba/alpha/string.hpp"
-#include "mamba/alpha/tuple.hpp"
--- cpp/include/alpha/mamba/mamba.hpp
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-#include "mamba/alpha/builtins.hpp"
