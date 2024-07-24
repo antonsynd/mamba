8ec58b84624c3c23bddf2172912b6d93dfd7d889
antonsynd <nguyen.antony@gmail.com>
2024-07-23 20:31:57 -0400

    str: Add some basic things

--- cpp/include/mamba/str.hpp
+++ cpp/include/mamba/str.hpp
@@ -2,8 +2,19 @@
 
 #include <string>
 
+#include "mamba/int.hpp"
+
 namespace mamba::builtins {
 
+// TODO: Make it Unicode friendly
 using str_t = std::string;
+
+namespace str {
+
+int_t Len(const str_t& s) {
+  return s.size();
+}
+
+}  // namespace str
 
 }  // namespace mamba::builtins
