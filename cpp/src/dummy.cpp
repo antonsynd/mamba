458366746452761a875ea8a8f6a944fdc65bd3b1
antonsynd <nguyen.antony@gmail.com>
2024-07-24 00:04:38 -0400

    src: Fix GTest

--- cpp/src/dummy.cpp
+++ cpp/src/dummy.cpp
@@ -1,1 +1,11 @@
-namespace mamba::builtins {}
+#include <iostream>
+
+namespace mamba::builtins {
+
+constexpr auto kDummy = 0;
+
+void foobar(int i) {
+  std::cout << i << std::endl;
+}
+
+}  // namespace mamba::builtins
