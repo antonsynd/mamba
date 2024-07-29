e838afd01909c9e8e5511816616e782bf578a75c
antonsynd <nguyen.antony@gmail.com>
2024-07-28 23:51:37 -0400

    iterator: Add commented-out boilerplate for C++ iterator API

--- cpp/include/mamba/iterator.hpp
+++ cpp/include/mamba/iterator.hpp
@@ -6,12 +6,27 @@
 
 namespace mamba::builtins {
 
+// template <typename T>
+// class iterator_t {
+//  public:
+//   virtual T operator*() const = 0;
+//   virtual T* operator->() const = 0;
+//   virtual iterator_t<T>& operator++() = 0;
+//   virtual iterator_t<T>& operator++(int) = 0;
+
+//   virtual iterator_t<T> operator+(int) = 0;
+//   virtual iterator_t<T>& operator+=(int) = 0;
+// };
+
 // TODO
 template <types::Sequence T>
 class Iterator {
  public:
   virtual Iterator<T> Iter() const = 0;
   virtual T::value_t Next() = 0;
+
+  // virtual iterator_t<T::value_t> begin() = 0;
+  // virtual iterator_t<T::value_t> end() = 0;
 };
 
 template <typename T>
