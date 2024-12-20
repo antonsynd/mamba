6e93bb2afdcac8a0137040dccd14b73ac780bcb4
antonsynd <nguyen.antony@gmail.com>
2024-12-19 19:20:58 -0500

    traits: Attempt nicer traits implementation

--- cpp/include/mamba/builtins/__types/bool.hpp
+++ cpp/include/mamba/builtins/__types/bool.hpp
@@ -11,8 +11,31 @@ namespace mamba::builtins::details {
 /// Int and Bool.
 using Bool = bool;
 
+// struct BoolDelegate {
+//  public:
+//   using value_type = Bool;
+
+//   static inline Str __Name__(const value_type) { return "bool"; }
+
+//   static inline Str __Repr__(const value_type v) {
+//     return v ? "True" : "False";
+//   };
+
+//   static inline Str __Str__(const value_type v) { return __Repr__(v); }
+
+//   static inline Bool __Bool__(const value_type v) { return v; }
+
+//   static inline Bool __Eq__(const value_type lhs, const value_type rhs) {
+//     return lhs == rhs;
+//   }
+
+//   static inline BigInt __Id__(const value_type) { return -1; }
+// };
+
+// template <Bool>
 template <>
 struct Traits<Bool> {
+  // using delegate_type = BoolDelegate;
   static constexpr std::string_view kName = "bool";
 };
 
--- cpp/include/mamba/builtins/__types/traits.hpp
+++ cpp/include/mamba/builtins/__types/traits.hpp
@@ -1,10 +1,31 @@
 #pragma once
 
+// #include "mamba/builtins/__concepts/value.hpp"
+
 namespace mamba::builtins::details {
 
+// template <Value T>
 template <typename T>
 struct Traits {
   static_assert(false, "Traits unimplemented");
 };
+
+// template <NotValue T>
+// struct Traits {
+//  public:
+//   using value_type = T;
+
+//   struct ObjectDelegate {
+//    public:
+//     static inline Str __Name__(const value_type& v) { return v.__Name__(); }
+//     static inline Str __Repr__(const value_type& v) { return v.__Repr__(); };
+//     static inline Str __Str__(const value_type& v) { return __Repr__(v); }
+//     static inline Bool __Bool__(const value_type& v) { return v.__Bool__(); }
+//     static inline BigInt __Id__(const value_type& v) { return v.__Id__(); }
+//     static inline Bool __Eq__(const value_type& lhs, const value_type& rhs) {
+//       return lhs.__Eq__(rhs);
+//     }
+//   };
+// };
 
 }  // namespace mamba::builtins::details
